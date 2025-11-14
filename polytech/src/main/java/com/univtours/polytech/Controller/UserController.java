73e006ed67c6663f94cecd3847aaca88352e725a
aljowld <m.aljoscha.wild@gmail.com>
2025-11-14 15:35:49 +0100

    ajout de : UserController. Motif : Utilisateur

--- polytech/src/main/java/com/univtours/polytech/Controller/UserController.java
+++ polytech/src/main/java/com/univtours/polytech/Controller/UserController.java
@@ -1,7 +1,79 @@
 package com.univtours.polytech.Controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.univtours.polytech.dto.UtilisateurDTO;
+import com.univtours.polytech.entity.Utilisateur;
+import com.univtours.polytech.mapper.UtilisateurMapper;
+import com.univtours.polytech.services.UserService;
+
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UtilisateurMapper utilisateurMapper;
+
+    // CREATE
+    @PostMapping
+    public ResponseEntity<UtilisateurDTO> createUser(@RequestBody UtilisateurDTO dto) {
+        Utilisateur entity = utilisateurMapper.toEntity(dto);
+        userService.createUser(entity);
+        UtilisateurDTO response = utilisateurMapper.toDTO(entity);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    // READ unitaire
+    @GetMapping("/{id}")
+    public ResponseEntity<UtilisateurDTO> getUser(@PathVariable int id) {
+        Utilisateur entity = userService.readUser(id);
+        UtilisateurDTO dto = utilisateurMapper.toDTO(entity);
+        return ResponseEntity.ok(dto);
+    }
+
+    // READ liste
+    @GetMapping
+    public ResponseEntity<List<UtilisateurDTO>> getAllUsers() {
+        List<Utilisateur> entities = userService.readAllUsers();
+        List<UtilisateurDTO> dtos =
+                entities.stream()
+                        .map(utilisateurMapper::toDTO)
+                        .toList();
+
+        return ResponseEntity.ok(dtos);
+    }
+
+    // UPDATE
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateUser(
+            @PathVariable int id,
+            @RequestBody UtilisateurDTO dto
+    ) {
+        Utilisateur entity = utilisateurMapper.toEntity(dto);
+        userService.updateUser(
+                id,
+                entity.getNom(),
+                entity.getPrenom(),
+                entity.getMail(),
+                entity.getPassword(),
+                entity.getUsername()
+        );
+        return ResponseEntity.noContent().build();
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable int id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
 }
