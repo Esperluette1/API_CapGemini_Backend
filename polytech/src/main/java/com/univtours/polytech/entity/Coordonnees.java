da0ce9574f23131f8b6966f7d6d0cb7b35a83e8b
Adam <ad.etude01@gmail.com>
2025-11-14 14:48:47 +0100

    nettoyage des getters et settes

--- polytech/src/main/java/com/univtours/polytech/entity/Coordonnees.java
+++ polytech/src/main/java/com/univtours/polytech/entity/Coordonnees.java
@@ -25,28 +25,4 @@ public class Coordonnees {
     private int id;
     private float longitude;
     private float latitude;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public float getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(float longitude) {
-        this.longitude = longitude;
-    }
-
-    public float getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(float latitude) {
-        this.latitude = latitude;
-    }
 }
--- polytech/src/main/java/com/univtours/polytech/entity/Terrain.java
+++ polytech/src/main/java/com/univtours/polytech/entity/Terrain.java
@@ -22,44 +22,4 @@ public class Terrain {
     private int quantite;
     private String description;
     private int coordonnees_id;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public int getQuantite() {
-        return quantite;
-    }
-
-    public void setQuantite(int quantite) {
-        this.quantite = quantite;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getCoordonnees_id() {
-        return coordonnees_id;
-    }
-
-    public void setCoordonnees_id(int coordonnees_id) {
-        this.coordonnees_id = coordonnees_id;
-    }
 }
