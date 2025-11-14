0f9f2852e57bb7e7f33de9cbbc57830d2591e838
aljowld <m.aljoscha.wild@gmail.com>
2025-11-14 14:57:37 +0100

    ajout de : ReservationRepository, ReservationService

---
+++ polytech/src/main/java/com/univtours/polytech/services/ReservationService.java
@@ -0,0 +1,60 @@
+package com.univtours.polytech.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.univtours.polytech.entity.Reservation;
+import com.univtours.polytech.repository.ReservationRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class ReservationService {
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    // Create
+    public void createReservation(Reservation Reservation) {
+        reservationRepository.save(Reservation);
+    }
+
+    // Read Unitaire
+    public Reservation readReservation(int ID) {
+        Optional<Reservation> reservation = reservationRepository.findById(ID);
+        if (reservation.isEmpty()) {
+            throw new EntityNotFoundException("Reservation non trouvé");
+        }
+        return reservation.get();
+    }
+
+    // Read par Liste
+    public List<Reservation> readAllReservations() {
+        return reservationRepository.findAll();
+    }
+
+    // Update
+
+    private int utilisateur_id;
+    private int terrain_id;
+    private int reservation;
+    public void updateReservation(int user_ID, int terrain_ID, int reservation_ID) {
+        Optional<Reservation> Reservation = reservationRepository.findById(reservation_ID);
+        if (Reservation.isEmpty()) {
+            throw new EntityNotFoundException("Reservation non trouvé");
+        }
+        Reservation.get().setReservation(reservation_ID);
+        Reservation.get().setUtilisateur_id(user_ID);
+        Reservation.get().setTerrain_id(terrain_ID);
+    }
+
+    // Delete
+    public void deleteReservation(int ID) {
+        if (!reservationRepository.existsById(ID)) {
+            throw new EntityNotFoundException("La reservation n'existe pas");
+        }
+        reservationRepository.deleteById(ID);
+    }
+}
